a9475211c042a18a7f439475c7d046b8b439f044
Loschen <u201714781@hust.edu.cn>
2021-04-16 17:39:16 +0800

    add several methods of converting csi matrix to image

--- preprocess.py
+++ preprocess.py
@@ -24,28 +24,51 @@ def load_csi(path, num_tones = 114, nc = 2, nr = 3) :
     #相位矩阵
     csi_series = obj['csi']
     csi_phase = np.empty((0, 2, 3, 114))
+    csi_amp = np.empty((0, 2, 3, 114))
     for csi in csi_series :
         csi = csi.transpose()
         angle_csi_temp = np.angle(csi)
+        amp_csi_temp = abs(csi)
         csi_phase = np.concatenate((csi_phase, np.expand_dims(angle_csi_temp, axis=0)))
+        csi_amp = np.concatenate((csi_amp, np.expand_dims(amp_csi_temp, axis=0)))
 
-    return csi_phase, csi_time
+    return csi_phase, csi_time, csi_amp
 
-#构造相位差矩阵
-def diff_phase_matrix(csi_phase, csi_time) :
+def get_mValue(csi_matrix) : #3*114*1000
+    minValue = csi_matrix[0][0][0]
+    maxValue = csi_matrix[0][0][0]
+    for i in range(3):
+        for j in range(114):
+            mintemp = min(csi_matrix[i][j])
+            maxtemp = max(csi_matrix[i][j])
+            if (minValue > mintemp) :
+                minValue = mintemp
+            if (maxValue < maxtemp) :
+                maxValue = maxtemp
+    return maxValue, minValue
+
+#构造相位差矩阵和幅度积矩阵(共轭相乘矩阵)
+def diff_phase_matrix(csi_phase, csi_time, csi_amp) :
     phase_temp = np.unwrap(csi_phase, axis=1).transpose() #114*3*2*n
+    amp_temp = csi_amp.transpose()#114*3*2*n
     interval = csi_time[-1] / 1000
     csi_time = csi_time / interval
     xnew = np.array(range(1000))
     csi_phase_temp = np.empty((114, 3, 2, 1000))
+    csi_amp_temp = np.empty((114,3,2,1000))
     for i in range(114) :
         for j in range(3) :
             for k in range(2) :
                 ff = interpolate.interp1d(csi_time, phase_temp[i][j][k])
+                ff2 = interpolate.interp1d(csi_time, amp_temp[i][j][k])
                 csi_phase_temp[i][j][k] = ff(xnew)
+                csi_amp_temp[i][j][k] = ff2(xnew)
     csi_matrix_temp = csi_phase_temp.transpose()
+    csi_amatrix_temp = csi_amp_temp.transpose()
     csi_matrix = np.empty((1000, 114, 3))
+    csi_amatrix = np.empty((1000, 114, 3))
     matrix_temp = np.empty((3, 114))
+    amatrix_temp = np.empty((3, 114))
     for i in range(1000) :
         for j in range(3) :
             for k in range(114) :
@@ -56,8 +79,12 @@ def diff_phase_matrix(csi_phase, csi_time) :
                     matrix_temp[j][k] = diff_temp - 2*np.pi
                 else:
                     matrix_temp[j][k] = diff_temp + 2*np.pi
+                am_temp = csi_amatrix_temp[i][0][j][k] * csi_amatrix_temp[i][1][j][k]
         csi_matrix[i] = matrix_temp.transpose()
-    return csi_matrix
+        csi_amatrix[i] = am_temp.transpose()
+    maxv, minv = get_mValue(csi_amatrix.transpose())
+    csi_amatrix = (csi_amatrix - minv) / (maxv - minv)
+    return csi_matrix, csi_amatrix
 
 # 相位差矩阵转图片
 def convertToImg(path, csi_matrix) :
@@ -65,16 +92,47 @@ def convertToImg(path, csi_matrix) :
     plt.imsave(path, img_matrix)
     return 
 
+#幅度矩阵
+def convertToImgC(path, csi_amatrix):
+    plt.imsave(path, csi_amatrix)
+    return
+
+#C1,C2:phase, C3:amp
+def convertToImgA(path, csi_matrix, csi_amatrix) :
+    img_matrix = np.empty((3, 114, 1000))
+    csi_matrix_temp = (csi_matrix + np.pi) / (2 * np.pi)
+    img_matrix[0] = csi_matrix_temp.transpose()[0]
+    img_matrix[1] = csi_matrix_temp.transpose()[1]
+    img_matrix[2] = csi_amatrix.transpose()[2]
+    plt.imsave(path, img_matrix.transpose())
+    return
+
+#C1:phase, C2,C3:amp
+def convertToImgB(path, csi_matrix, csi_amatrix) :
+    img_matrix = np.empty((3, 114, 1000))
+    csi_matrix_temp = (csi_matrix + np.pi) / (2 * np.pi)
+    img_matrix[0] = csi_matrix_temp.transpose()[0]
+    img_matrix[1] = csi_amatrix.transpose()[1]
+    img_matrix[2] = csi_amatrix.transpose()[2]
+    plt.imsave(path, img_matrix.transpose())
+    return
+
 #批量转化
 def batch_convert(mode, begin, end) :
     for i in range(begin, end, 2) :
         src_path = '3_12_after/' + mode + '/csi/data_' + str(i) + '.csi'
         dst_path = 'img/' + mode + '/data_' + str(i) + '.jpeg'
+        dst_pathA = 'imgA/' + mode + '/data_' + str(i) + '.jpeg'
+        dst_pathB = 'imgB/' + mode + '/data_' + str(i) + '.jpeg'
+        dst_pathC = 'imgC/' + mode + '/data_' + str(i) + '.jpeg'
         print('processing ' + src_path)
-        csi_phase, csi_time = load_csi(src_path)
-        csi_matrix = diff_phase_matrix(csi_phase, csi_time)
+        csi_phase, csi_time, csi_amp = load_csi(src_path)
+        csi_matrix, csi_amatrix = diff_phase_matrix(csi_phase, csi_time, csi_amp)
         #matrix = (csi_matrix + np.pi) / (2 * np.pi)
-        convertToImg(dst_path, csi_matrix)
+        #convertToImg(dst_path, csi_matrix) #DOWN!
+        #convertToImgC(dst_pathC, csi_amatrix)#DOWN!
+        convertToImgA(dst_pathA, csi_matrix, csi_amatrix)
+        convertToImgB(dst_pathB, csi_matrix, csi_amatrix)
     return 
 
 #批量压缩114*114

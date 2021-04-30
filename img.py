a60bbfa1bc488c01c6277ab0d15b2455859dc142
Loschen <u201714781@hust.edu.cn>
2021-04-30 11:57:31 +0800

    add orignal data picture

--- img.py
+++ img.py
@@ -8,6 +8,19 @@ phase, time, amplitude = load_csi('3_12_after/left/csi/data_0.csi')
 phase_matrix, amp_matrix = diff_phase_matrix(phase, time, amplitude)
 path = 'gesture_recognition/img/'
 plt.subplot(211)
+plt.ylabel('amplitude')
+plt.xticks([])
+plt.plot(time, amplitude.transpose()[0][0][0])
+
+plt.subplot(212)
+plt.ylabel('phase')
+plt.xlabel('time')
+plt.plot(time, phase.transpose()[0][0][0])
+impath=path + 'data.jpeg'
+plt.savefig(impath, dpi=600)
+plt.show()
+'''
+plt.subplot(211)
 #plt.xlabel('time')
 plt.ylabel('amplitude')
 plt.xticks([])
@@ -30,3 +43,4 @@ for i in range(3):
 impath = path + 'butter.jpeg'
 plt.savefig(impath, dpi=600)
 plt.show()
+'''
